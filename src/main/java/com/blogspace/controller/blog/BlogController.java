65096f509e43639e136871bdf0b2f93497478ff8
rohith-sura <surarohith97@gmail.com>
2023-10-10 20:42:32 -0400

    "Added sample method for test"

--- src/main/java/com/blogspace/controller/blog/BlogController.java
+++ src/main/java/com/blogspace/controller/blog/BlogController.java
@@ -1,9 +1,16 @@
 package com.blogspace.controller.blog;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/blog/v1")
 public class BlogController {
+
+    @GetMapping("/sample")
+    public String sample()
+    {
+        return "Hello";
+    }
 }
